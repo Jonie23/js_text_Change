4c1145cd3047f9b27984c8bd51ffb295cc8d51b3
Jones23 <quarteyjonie18@gmail.com>
2020-01-02 15:02:11 +0000

    first commit

---
+++ text.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    document.getElementById('changeText').style.color = 'blue';
+}
+
+function oldColor(){
+    document.getElementById('changeText').style.color = 'black';
+}
